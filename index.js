06ff5ba54b9843773136ce7920673a2a29477b8f
Aubrey Crook <aubreycrook@hotmail.com>
2022-01-05 18:29:45 -0500

    Added a;; featureds required for Readme file.

---
+++ index.js
@@ -0,0 +1,109 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+const path = require("path")
+
+async function CreateReadMe(){
+    let readmedata = {
+        
+    }
+    readmedata.title = await getUserInput("enter the title:")
+    readmedata.description = await getUserInput("enter description:")
+    readmedata.installation = await getUserInput("how does the user install this:")
+    readmedata.usage = await getUserInput("what is this project used for:")
+    readmedata.license = await getUserchoice(
+        "choice a license",
+        [ 
+            "GNU GPLv3", 
+            "GNU LGPLv3",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "MIT License",
+            "Boost Software License 1.0",
+            "The Unlicense",
+            "none"
+
+
+        ]
+    )
+    readmedata.contribute = await getUserInput("how can people contribute to the project:")
+    //test
+    //question
+
+    readmedata.test = await getUserInput("Enter instructions for testing this application")
+    readmedata.username = await getUserInput("Enter github username")
+    readmedata.email = await getUserInput("Enter email for questions")
+
+
+
+    var content = `# ${readmedata.title} 
+
+## Description
+
+${readmedata.description} 
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribute](#contribute)
+
+## Installation Instructions
+
+${readmedata.installation}
+
+## Usage Information
+
+${readmedata.usage} 
+
+## License Information
+
+${readmedata.license}
+
+## Contribution Guidelines
+
+${readmedata.contribute}
+
+## Test Instructions
+
+${readmedata.test}
+
+## Questions 
+
+Please contact the email below with questions
+
+github username: ${readmedata.username}
+
+email: ${readmedata.email}` 
+
+
+
+
+    console.log(content)
+
+    fs.writeFileSync(path.join(__dirname,"readme.md"),content)
+} CreateReadMe()
+
+async function getUserInput(message){
+
+    return (await inquirer.prompt({
+        type: "input", 
+        name: "response",
+        message: message,
+        
+            })).response;
+
+
+} 
+async function getUserchoice(message,choices){
+
+    return (await inquirer.prompt({
+        type: "list",
+        name: "response",
+        message: message,
+        choices: choices
+
+            })).response;
+
+
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Cheap 
+
+## Description
+
+srth 
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribute](#contribute)
+
+## Installation Instructions
+
+sdfg
+
+## Usage Information
+
+sdgf 
+
+## License Information
+
+Mozilla Public License 2.0
+
+## Contribution Guidelines
+
+2
+
+## Test Instructions
+
+frgrgf
+
+## Questions 
+
+Please contact the email below with questions
+
+github username: ty 
+email: hotmail
